87be4f9c213f114aaf6360a557d4fe97f02b68c8
yuppy-0306 <205904079+yuppy-0306@users.noreply.github.com>
2025-06-16 15:53:26 +0900

    prettier

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,11 +1,10 @@
-for (let i = 1; i <= 20; i++) 
-{
+for (let i = 1; i <= 20; i++) {
   if (i % 15 === 0) {
-    console.log('FizzBuzz');
+    console.log("FizzBuzz");
   } else if (i % 3 === 0) {
-    console.log('Fizz');
+    console.log("Fizz");
   } else if (i % 5 === 0) {
-    console.log('Buzz');
+    console.log("Buzz");
   } else {
     console.log(i);
   }
