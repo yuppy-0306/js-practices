07f426e57db9e33624978eddc5814d4ed5bcceb3
yuppy-0306 <205904079+yuppy-0306@users.noreply.github.com>
2025-06-18 16:24:35 +0900

    年月表示

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -1,4 +1,14 @@
 function displayCalendar(year, month) {
+  const firstDay = new Date(year, month - 1, 1);
+  const lastDay = new Date(year, month, 0);
+
+  console.log(`       ${month}月 ${year}`);
+  console.log("日 月 火 水 木 金 土");
+
+  const firstDayOffset = firstDay.getDay();
+
+  process.stdout.write('   '.repeat(firstDayOffset));
+
   
   console.log();
 }
