6310c8bc936445c065d3c6984a07d3264004b1f0
yuppy-0306 <205904079+yuppy-0306@users.noreply.github.com>
2025-06-18 11:52:06 +0900

    FizzBuzz条件分岐

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,7 +1,15 @@
 function fizzbuzz() {
     let output = '';
     for (let i = 1; i <= 20; i++) {
-        
+        if (i % 15 === 0) {
+            output += 'FizzBuzz\n';
+        } else if (i % 3 === 0) {
+            output += 'Fizz\n';
+        } else if (i % 5 === 0) {
+            output += 'Buzz\n';
+        } else {
+            output += i + '\n';
+        }
     }
     console.log(output);
 }
