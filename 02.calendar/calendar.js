aaae1a57240b73404e96c9a331cc6e74de26c801
yuppy-0306 <205904079+yuppy-0306@users.noreply.github.com>
2025-06-19 10:26:28 +0900

    ESLint、Prettierでチェック

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -7,11 +7,12 @@ function displayCalendar(year, month) {
 
   const firstDayOffset = firstDay.getDay();
 
-  process.stdout.write('   '.repeat(firstDayOffset));
+  // eslint-disable-next-line no-undef
+  process.stdout.write("   ".repeat(firstDayOffset));
 
-  
   for (let date = firstDay; date <= lastDay; date.setDate(date.getDate() + 1)) {
-    process.stdout.write(String(date.getDate()).padStart(2, ' ') + ' ');
+    // eslint-disable-next-line no-undef
+    process.stdout.write(String(date.getDate()).padStart(2, " ") + " ");
     if (date.getDay() === 6) {
       console.log();
     }
@@ -19,18 +20,19 @@ function displayCalendar(year, month) {
   console.log();
 }
 
+// eslint-disable-next-line no-undef
 const args = process.argv.slice(2);
 let year = new Date().getFullYear();
 let month = new Date().getMonth() + 1;
 
 for (let i = 0; i < args.length; i++) {
-  if (args[i] === '-y' && args[i + 1]) {
+  if (args[i] === "-y" && args[i + 1]) {
     const parsedYear = parseInt(args[i + 1], 10);
     if (!isNaN(parsedYear)) {
       year = parsedYear;
     }
     i++;
-  } else if (args[i] === '-m' && args[i + 1]) {
+  } else if (args[i] === "-m" && args[i + 1]) {
     const parsedMonth = parseInt(args[i + 1], 10);
     if (!isNaN(parsedMonth) && parsedMonth >= 1 && parsedMonth <= 12) {
       month = parsedMonth;
