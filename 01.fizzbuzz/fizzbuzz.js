f0738f544df064409e363acc14d4e3fa0f987002
yuppy-0306 <205904079+yuppy-0306@users.noreply.github.com>
2025-06-23 13:46:22 +0900

    テンプレート文字列

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,14 +1,14 @@
 function fizzbuzz() {
   let output = "";
   for (let i = 1; i <= 20; i++) {
-    if (i % 15 === 0) {
-      output += "FizzBuzz\n";
+    if (i % 3 === 0 && i % 5 === 0) {
+      output += `FizzBuzz\n`;
     } else if (i % 3 === 0) {
-      output += "Fizz\n";
+      output += `Fizz\n`;
     } else if (i % 5 === 0) {
-      output += "Buzz\n";
+      output += `Buzz\n`;
     } else {
-      output += i + "\n";
+      output += `${i}\n`;
     }
   }
   console.log(output);
